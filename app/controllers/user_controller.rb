c16e132b208347750ad1c3bdc46b33f098980900
charlie763 <cwisoff@gmail.com>
2020-07-06 13:59:10 -0600

    login workflow

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,17 @@ class UserController < ApplicationController
     erb :'users/login'
   end
 
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/'
+    else
+      @invalid_input = true
+      erb :'users/login'
+    end
+  end
+
   get '/signup' do
     erb :'users/signup'
   end
