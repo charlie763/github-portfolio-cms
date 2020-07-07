e4db082a9465e42297d4a6af5a5b10c9b62f18eb
charlie763 <cwisoff@gmail.com>
2020-07-07 09:47:40 -0600

    new portfolio and edit portfolio controllers

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -9,8 +9,17 @@ class PortfolioController < ApplicationController
   get '/portoflios/new' do
     redirect_if_not_user
     
-    @user = User.find(session[:user_id])
     erb :'portfolios/new'
+  end
+
+  post '/portfolios' do
+    redirect_if_not_user
+
+    @portfolio = Portfolio.new(params)
+    @portfolio.user = User.find_by(session[:user_id])
+    @portfolio.save
+
+    erb :"portfolios/#{@portfolio.id}/edit"
   end
 
   get '/portoflios/:id' do
@@ -19,6 +28,25 @@ class PortfolioController < ApplicationController
 
   get '/portoflios/:id/edit' do
     redirect_if_not_user
+
+    user = User.find_by(session[:user_id])
+    @portfolio = Portfolio.find_by(user: user)
+    if !@portfolio.repos.empty?
+      @repos = @portfolio.repos
+    elsif repos = Repo.find_by(user: user)
+      @repos = repos
+    else
+      github_repos = GithubApiResponse.new(github_username: user.github_username).get_repos
+      @repos = github_repos.map{|repo_hash| Repo.create(
+        name: repo_hash["name"],
+        github_url: repo_hash["html_url"],
+        description: repo_hash["description"],
+        created_at: repo_hash["created_at"],
+        updated_at: repo_hash["updated_at"]
+      )}
+    end
+
+    erb :'portfolios/edit'
   end
 
   patch '/portoflios/:id' do
