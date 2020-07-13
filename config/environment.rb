ae5800e5bb39cdbec22a453d0877b727759cda3e
charlie763 <cwisoff@gmail.com>
2020-07-13 09:57:41 -0600

    configuring db for production

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,14 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-set :database, {adapter: "sqlite3", database: "db/#{ENV['SINATRA_ENV']}.sqlite"}
+configure :development do
+  set :database, {adapter: "sqlite3", database: "db/#{ENV['SINATRA_ENV']}.sqlite"}
+end
+
+configure :production do
+  set :database, {
+    adapter: "postgresql", database: "postgres://wppaachukvkzbu:30928a169e4edb65be8f89557ae74c43e77cdd8f2f2b4557fe824cb28f2675bd@ec2-52-202-66-191.compute-1.amazonaws.com:5432/db009ve2i4trao"}
+  }
+end
 
 require_all 'app'
